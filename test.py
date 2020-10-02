719aed941975578f40ad2e2ae6543e5810e39068
Ali Ashraf <vip@Alis-MacBook-Pro.local>
2020-10-03 00:07:10 +0200

    python

---
+++ test.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+if __name__ == "__main__":
+    x = "ECE_180_DA_DB"
+    if x == "ECE_180_DA_DB":
+        print("You are living in 2020")
+    else:
+        x = x + "- Best class ever"
+        print(x)
+
+
+# In[ ]:
+
+
+
+
